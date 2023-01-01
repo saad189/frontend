abdd1623462f9eb8620ac506ce5ac3033c83569d
mahm <mahm@ciklum.com>
2023-01-02 02:04:42 +0500

    BoilerPlate

---
+++ src/Models/module-info.ts
@@ -0,0 +1,10 @@
+export const MODULE_ADDRESS = {
+    HOME: 'home',
+    SETTINGS: 'settings',
+    ABOUT_US: 'about'
+}
+
+export const MODULE_NAMES = {
+    [MODULE_ADDRESS.HOME]: 'Home',
+    [MODULE_ADDRESS.SETTINGS]: 'Settings',
+}
--- src/Modules/app/app-routing.module.ts
+++ src/Modules/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MODULE_ADDRESS } from 'src/Models/module-info';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: MODULE_ADDRESS.SETTINGS,
+  loadChildren: () => import('../settings/settings.module').then(m => m.SettingsModule)
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
